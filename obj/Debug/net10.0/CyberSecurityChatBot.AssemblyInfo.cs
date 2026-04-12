f659cd5ce8c3d6a2665e7ab47802a3ee43457af6
Chantel <ST10497253@rcconnect.edu.za>
2026-04-12 23:46:19 +0200

    Updated README.md

--- obj/Debug/net10.0/CyberSecurityChatBot.AssemblyInfo.cs
+++ obj/Debug/net10.0/CyberSecurityChatBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberSecurityChatBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ea742c29b6e2897556e688f7eddd8a66f2cb58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0afc6babf12e99e53163234eb7d97eb156f0f8d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberSecurityChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberSecurityChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
